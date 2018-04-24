f6030d0abfa7341b4e233da64b411ecd464a41ba
Carlos del Moral <cdelmoral@bluewolf.com>
2018-04-24 11:46:17 -0500

    Add db route

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var app = express();
+var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) { 
+    client.query('SELECT * FROM Account', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
